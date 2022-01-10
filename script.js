569ccbbfa1c684371a698e822cd3064b28de0640
Chue Chang <chuechang1996@outlook.com>
2022-01-10 04:27:23 -0600

    add current moment to main

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// var dropDownEl = document.getElementById("dropdown-nav")
-// dropDownEl.addEventListener("click",function(){
-//     dropDownEl.setAttribute("aria-expanded", "true")
-//     console.log(dropDownEl.attributes)
-// })
+// current moment display in main.html
+setInterval(function () {
+    var currentDisplayEl = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    document.getElementById('current-date-time').textContent = currentDisplayEl
+}, 0)
