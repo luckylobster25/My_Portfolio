1ec3afea5892feafda71784ea29f0a1e86917186
Chue Chang <chuechang1996@outlook.com>
2022-01-10 03:25:13 -0600

    add projects file

---
+++ script.js
@@ -0,0 +1,5 @@
+// var dropDownEl = document.getElementById("dropdown-nav")
+// dropDownEl.addEventListener("click",function(){
+//     dropDownEl.setAttribute("aria-expanded", "true")
+//     console.log(dropDownEl.attributes)
+// })
