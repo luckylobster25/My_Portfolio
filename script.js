ecd04b841bf258d72ca80f470a1fe9eb3ed5714f
Chue Chang <chuechang1996@outlook.com>
2022-01-13 17:14:52 -0600

    added project 5 with option to display img

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ setInterval(function () {
     var currentDisplayEl = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     document.getElementById('current-date-time').textContent = currentDisplayEl
 }, 0)
+
